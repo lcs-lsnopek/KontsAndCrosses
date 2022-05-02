f6e3a4c5457e413d09110faf5770c592fb42c0b9
Logan Snopek <LSnopek@lcs.on.ca>
2022-05-02 15:02:04 -0400

    Starting to organons the project

---
+++ KontsAndCrosses/Views/GameBordView.swift
@@ -0,0 +1,20 @@
+//
+//  GameBordView.swift
+//  KontsAndCrosses
+//
+//  Created by Logan Snopek on 2022-05-02.
+//
+
+import SwiftUI
+
+struct GameBordView: View {
+    var body: some View {
+        Text("This is the game bord")
+    }
+}
+
+struct GameBordView_Previews: PreviewProvider {
+    static var previews: some View {
+        GameBordView()
+    }
+}
