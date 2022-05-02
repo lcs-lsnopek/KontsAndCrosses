c4579cf0eef5486368aada13882b4cc56c5aba20
Logan Snopek <LSnopek@lcs.on.ca>
2022-05-02 14:58:32 -0400

    Initial Commit

---
+++ KontsAndCrosses/KontsAndCrossesApp.swift
@@ -0,0 +1,17 @@
+//
+//  KontsAndCrossesApp.swift
+//  KontsAndCrosses
+//
+//  Created by Logan Snopek on 2022-05-02.
+//
+
+import SwiftUI
+
+@main
+struct KontsAndCrossesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
